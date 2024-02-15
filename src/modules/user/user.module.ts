4f806bdd2a2ff8ceae872025be48cfb7482023b8
Luis A. Alvarado Z <luis_alejandro.az@outlook.com>
2024-02-14 22:00:20 -0400

    feat: configuring user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,12 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User, UserShema } from './schemas';
+import { UserRepository } from './repositories';
+import { SaveUserUseCase } from './useCases';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserShema }]),
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [SaveUserUseCase, UserRepository, UserService],
+  exports: [UserRepository],
 })
 export class UserModule {}

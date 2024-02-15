76755450a8e7c6fbf4389238147ae7dd3a5590e6
Luis A. Alvarado Z <luis_alejandro.az@outlook.com>
2024-02-14 21:32:15 -0400

    feat: configuring class validator

--- src/config/database/database.config.ts
+++ src/config/database/database.config.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 export const MongooseConfig: DynamicModule = MongooseModule.forRoot(
   process.env.MONGODB_HOST,
   {
-    dbName: process.env.MONGODB_NAME,
+    dbName: process.env.MONGODB_DBNAME,
     user: process.env.MONGODB_USER,
     pass: process.env.MONGODB_PASSWORD,
   },

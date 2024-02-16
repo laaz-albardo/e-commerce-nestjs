f9aa6953a2b2f8824f6e294b0a538e1fece56fa6
Luis A. Alvarado Z <luis_alejandro.az@outlook.com>
2024-02-15 22:17:40 -0400

    feat: creating new database config for unit test

--- src/config/database/database.config.ts
+++ src/config/database/database.config.ts
@@ -2,10 +2,18 @@ import { DynamicModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import 'dotenv/config';
 
-export const MongooseConfig: DynamicModule = MongooseModule.forRoot(
-  process.env.MONGODB_HOST,
+const dbHost = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
+
+export const MongooseConfig: DynamicModule = MongooseModule.forRoot(dbHost, {
+  dbName: process.env.MONGODB_DBNAME,
+  user: process.env.MONGODB_USER,
+  pass: process.env.MONGODB_PASSWORD,
+});
+
+export const MongooseConfigTest: DynamicModule = MongooseModule.forRoot(
+  dbHost,
   {
-    dbName: process.env.MONGODB_DBNAME,
+    dbName: process.env.MONGODB_DBTEST,
     user: process.env.MONGODB_USER,
     pass: process.env.MONGODB_PASSWORD,
   },

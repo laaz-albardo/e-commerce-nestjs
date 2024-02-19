932590e89b043bee9aed465d668b10fce8eddcc2
Luis A. Alvarado Z <luis_alejandro.az@outlook.com>
2024-02-19 13:15:07 -0400

    feat: creating auth module and jwt config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseConfig } from './config';
-import { UserModule } from './modules';
+import { AuthModule, UserModule } from './modules';
 
 @Module({
   imports: [
@@ -11,6 +11,7 @@ import { UserModule } from './modules';
     }),
     MongooseConfig,
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}

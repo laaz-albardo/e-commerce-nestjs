0377181f5d645ae0956af2c63b1733e9e6fd86ea
Luis A. Alvarado Z <luis_alejandro.az@outlook.com>
2024-02-25 11:59:25 -0400

    feat: adding validations for role and new validations for password in user dto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,13 +1,16 @@
 import { IUser } from '../interfaces';
 import {
   IsEmail,
+  IsEnum,
   IsString,
+  Matches,
   MaxLength,
   MinLength,
   ValidateNested,
 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreatePersonDto } from './create-person.dto';
+import { UserRoleEnum } from '../enums';
 
 export class CreateUserDto implements Partial<IUser> {
   @IsEmail()
@@ -17,7 +20,19 @@ export class CreateUserDto implements Partial<IUser> {
   @IsString()
   @MinLength(8)
   @MaxLength(50)
+  @Matches(/[A-Z]/, {
+    message: 'password must have at least one capital letter',
+  })
+  @Matches(/[a-z]/, {
+    message: 'password must have at least one lowercase letter',
+  })
+  @Matches(/\d/, {
+    message: 'password must have at least one number',
+  })
   password: string;
+
+  @IsEnum(UserRoleEnum)
+  role: UserRoleEnum;
 
   @ValidateNested()
   @Type(() => CreatePersonDto)

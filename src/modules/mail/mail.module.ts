5a2f064e14ac2902bc5c5c2766dbf7c19822ddf4
Luis A. Alvarado Z <luis_alejandro.az@outlook.com>
2024-03-11 16:10:50 -0400

    feat: adding nest mailer config to mail module

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
-import { MailService } from './mail.service';
-import { MailController } from './mail.controller';
+import { ConfigService } from '@nestjs/config';
+import { MailerModule, MailerOptions } from '@nestjs-modules/mailer';
+import { ActivateAccountListener } from './listeners';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
 
 @Module({
-  controllers: [MailController],
-  providers: [MailService],
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService): MailerOptions => {
+        return {
+          transport: {
+            host: config.getOrThrow('SMTP_HOST'),
+            port: config.getOrThrow('SMTP_PORT'),
+            secure: config.getOrThrow('SMTP_SECURE_SSL') === 'true',
+            auth: {
+              user: config.getOrThrow('SMTP_USERNAME'),
+              pass: config.getOrThrow('SMTP_PASSWORD'),
+            },
+          },
+          defaults: {
+            from: `"No Reply" <${config.getOrThrow('SMTP_SENDER_EMAIL_DEFAULT')}>`,
+          },
+          template: {
+            dir: join(__dirname, 'templates'),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      },
+    }),
+  ],
+  providers: [ActivateAccountListener],
 })
 export class MailModule {}
